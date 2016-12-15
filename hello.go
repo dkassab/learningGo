64589de4ac70381d4f804d4bd4ef0ad004adb5ac
Dima Kassab <dkassab@dkassab-mac.local>
2016-12-15 16:09:37 -0500

    adding and printing some data using tablewriter

--- hello.go
+++ hello.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/olekukonko/tablewriter"
 
 func main() {
     fmt.Printf("hello, world\n")
     fmt.Println("مرحبا من ديمة!")
+
+    data:=[][]string{
+		[]string{"dima","go","4"},
+		[]string{"junaid","go","0"},
+    }
+
+    table:=tablewriter.NewWriter(os.Stdout)
+    table.SetHeader([]string{"Name","PL", "Level"})
+    table.AppendBulk(data)
+    table.Render()
 }
